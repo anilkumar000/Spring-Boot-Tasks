4057a0bdb614e8ef48d4d562bd29ded61b5d98a5
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-08-06 11:39:40 +0530

    repository layer and service layer testcases written

---
+++ src/test/java/com/stackroute/trackservice/controller/TrackControllerTest.java
@@ -0,0 +1,33 @@
+package com.stackroute.trackservice.controller;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class TrackControllerTest {
+
+    @Test
+    public void saveTrack() {
+    }
+
+    @Test
+    public void getTrackById() {
+    }
+
+    @Test
+    public void deleteTrackById() {
+    }
+
+    @Test
+    public void getAllTracks() {
+    }
+
+    @Test
+    public void updateTrack() {
+    }
+
+    @Test
+    public void selectTrackByName() {
+    }
+}
